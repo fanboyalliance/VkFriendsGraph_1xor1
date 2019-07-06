ca75ee1df3c434b55cb5be51bd5d057e58aa2f1f
fanboyalliance <alex.raswqa@gmail.com>
2019-07-06 21:45:06 +0300

    improve response models

---
+++ src/main/java/infrastructure/Models/MutualFriends.java
@@ -0,0 +1,41 @@
+package infrastructure.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MutualFriends {
+    private long id;
+    private List<Long> commonFriendsIds;
+    private long commonCount;
+
+    @JsonSetter("id")
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public List<Long> getCommonFriendsIds() {
+        return commonFriendsIds;
+    }
+
+    public long getCommonCount() {
+        return commonCount;
+    }
+
+    @JsonSetter("common_friends")
+    public void setCommonFriendsIds(ArrayList<Long> commonFriends) {
+        this.commonFriendsIds = commonFriends;
+    }
+
+    @JsonSetter("common_count")
+    public void setCommonCount(long commonCount) {
+        this.commonCount = commonCount;
+    }
+}
---
+++ src/main/java/infrastructure/Models/MutualFriendsContainer.java
@@ -0,0 +1,22 @@
+package infrastructure.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MutualFriendsContainer {
+    private List<MutualFriends> mutualFriends;
+
+    public List<MutualFriends> getMutualFriends() {
+        return mutualFriends;
+    }
+
+    @JsonSetter("response")
+    public void setMutualFriends(ArrayList<MutualFriends> mutualFriends) {
+        this.mutualFriends = mutualFriends;
+    }
+
+}
